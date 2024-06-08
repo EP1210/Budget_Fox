76519b4e9545c432d9acc054ba1fd006ab607be4
michael-mezgo <58122000+michael-mezgo@users.noreply.github.com>
2024-06-08 12:46:46 +0200

    Add legend and marker to the StatisticsScreen.kt #5

--- app/src/main/java/at/ac/fhcampuswien/budget_fox/screens/StatisticsScreen.kt
+++ app/src/main/java/at/ac/fhcampuswien/budget_fox/screens/StatisticsScreen.kt
@@ -1,8 +1,12 @@
 package at.ac.fhcampuswien.budget_fox.screens
 
+import android.graphics.Typeface
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Label
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -11,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import at.ac.fhcampuswien.budget_fox.view_models.StatisticsViewModel
 import at.ac.fhcampuswien.budget_fox.widgets.SimpleBottomNavigationBar
@@ -19,16 +24,32 @@ import com.patrykandpatrick.vico.compose.cartesian.CartesianChartHost
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberBottomAxis
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStartAxis
 import com.patrykandpatrick.vico.compose.cartesian.layer.rememberColumnCartesianLayer
+import com.patrykandpatrick.vico.compose.cartesian.marker.rememberDefaultCartesianMarker
 import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
 import com.patrykandpatrick.vico.compose.cartesian.rememberVicoZoomState
 import com.patrykandpatrick.vico.compose.common.component.rememberLineComponent
+import com.patrykandpatrick.vico.compose.common.component.rememberShapeComponent
+import com.patrykandpatrick.vico.compose.common.component.rememberTextComponent
+import com.patrykandpatrick.vico.compose.common.of
+import com.patrykandpatrick.vico.compose.common.rememberHorizontalLegend
+import com.patrykandpatrick.vico.compose.common.rememberLegendItem
+import com.patrykandpatrick.vico.compose.common.vicoTheme
+import com.patrykandpatrick.vico.core.cartesian.CartesianDrawContext
+import com.patrykandpatrick.vico.core.cartesian.CartesianMeasureContext
 import com.patrykandpatrick.vico.core.cartesian.axis.AxisItemPlacer
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianValueFormatter
 import com.patrykandpatrick.vico.core.cartesian.data.columnSeries
 import com.patrykandpatrick.vico.core.cartesian.layer.ColumnCartesianLayer
+import com.patrykandpatrick.vico.core.cartesian.marker.DefaultCartesianMarker
+import com.patrykandpatrick.vico.core.common.Dimensions
+import com.patrykandpatrick.vico.core.common.shape.Shape
 import java.text.DateFormatSymbols
 import java.util.Locale
+
+/*
+ * Source: https://github.com/patrykandpatrick/vico/blob/master/sample/src/main/java/com/patrykandpatrick/vico/sample/showcase/charts/Chart3.kt
+ */
 
 @Composable
 fun StatisticsScreen(
@@ -37,14 +58,11 @@ fun StatisticsScreen(
     viewModel: StatisticsViewModel
 ) {
     viewModel.mapTransactionsFromUserToMonths(year = 2024)
-    // Help: https://github.com/patrykandpatrick/vico/blob/master/sample/src/main/java/com/patrykandpatrick/vico/sample/showcase/charts/Chart3.kt
     val modelProducer = remember { CartesianChartModelProducer.build() }
     val incomes by viewModel.incomes.collectAsState()
     val expenses by viewModel.expenses.collectAsState()
 
     modelProducer.tryRunTransaction {
-        /* Learn more:
-        https://patrykandpatrick.com/vico/wiki/cartesian-charts/layers/line-layer#data. */
         columnSeries {
             series(
                 y = incomes.values,
@@ -75,16 +93,28 @@ fun StatisticsScreen(
                 .padding(paddingValues = it)
                 .fillMaxSize()
         ) {
-            ComposeChart1(
-                modelProducer = modelProducer,
-                modifier = Modifier.fillMaxSize()
-            )
+            Row {
+            }
+            Row {
+                IncomeExpensesChart(
+                    modelProducer = modelProducer,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
     }
 }
 
 @Composable
-private fun ComposeChart1(modelProducer: CartesianChartModelProducer, modifier: Modifier) {
+private fun IncomeExpensesChart(modelProducer: CartesianChartModelProducer, modifier: Modifier) {
+    val label = rememberTextComponent()
+    val indicator = rememberShapeComponent(Shape.Pill, strokeWidth = 8.dp)
+
+    val marker = rememberDefaultCartesianMarker(
+        label = label,
+        labelPosition = DefaultCartesianMarker.LabelPosition.Top,
+        indicator = indicator,
+    )
     CartesianChartHost(
         chart =
         rememberCartesianChart(
@@ -101,6 +131,7 @@ private fun ComposeChart1(modelProducer: CartesianChartModelProducer, modifier: 
                     ),
                 )
             ),
+            legend = rememberLegend(),
             startAxis = rememberStartAxis(),
             bottomAxis =
             rememberBottomAxis(
@@ -111,15 +142,47 @@ private fun ComposeChart1(modelProducer: CartesianChartModelProducer, modifier: 
                 },
             ),
         ),
+        marker = marker,
         modelProducer = modelProducer,
         modifier = modifier,
         zoomState = rememberVicoZoomState(zoomEnabled = false),
     )
 }
 
+/*
+ * Source: https://github.com/patrykandpatrick/vico/blob/master/sample/src/main/java/com/patrykandpatrick/vico/sample/showcase/charts/Chart7.kt#L109
+ */
+@Composable
+private fun rememberLegend() =
+    rememberHorizontalLegend<CartesianMeasureContext, CartesianDrawContext>(
+        items =
+        chartColors.mapIndexed { index, chartColor ->
+            rememberLegendItem(
+                icon = rememberShapeComponent(Shape.Pill, chartColor),
+                label =
+                rememberTextComponent(
+                    color = vicoTheme.textColor,
+                    textSize = 12.sp,
+                    typeface = Typeface.MONOSPACE,
+                ),
+                labelText = (when (chartColor) {
+                    colorRed -> "Incomes"
+                    colorGreen -> "Expenses"
+                    else -> "Error"
+                })
+            )
+        },
+        iconSize = 8.dp,
+        iconPadding = 8.dp,
+        spacing = 4.dp,
+        padding = Dimensions.of(top = 8.dp),
+    )
+
 private const val COLUMN_THICKNESS_DP: Int = 10
 private val colorGreen = Color(0xFF008000)
 private val colorRed = Color(0xFFFF0000)
+
+private val chartColors = listOf(colorRed, colorGreen)
 
 private val monthNames = DateFormatSymbols.getInstance(Locale.US).shortMonths
 private val bottomAxisValueFormatter = CartesianValueFormatter { x, _, _ ->
