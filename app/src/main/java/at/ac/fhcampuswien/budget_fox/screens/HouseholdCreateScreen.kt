8fea954be2f91ed940708ec3687fb0d6df58eb25
michael-mezgo <58122000+michael-mezgo@users.noreply.github.com>
2024-06-13 12:13:48 +0200

    QR Code for joining household can be created
    #14, #15

--- app/src/main/java/at/ac/fhcampuswien/budget_fox/screens/HouseholdCreateScreen.kt
+++ app/src/main/java/at/ac/fhcampuswien/budget_fox/screens/HouseholdCreateScreen.kt
@@ -1,7 +1,9 @@
 package at.ac.fhcampuswien.budget_fox.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -9,13 +11,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import at.ac.fhcampuswien.budget_fox.view_models.HouseholdCreateViewModel
 import at.ac.fhcampuswien.budget_fox.widgets.SimpleButton
 import at.ac.fhcampuswien.budget_fox.widgets.SimpleField
 import at.ac.fhcampuswien.budget_fox.widgets.SimpleTopAppBar
+import com.lightspark.composeqr.QrCodeView
 
 @Composable
 fun HouseholdCreateScreen(
@@ -51,6 +56,15 @@ fun HouseholdCreateScreen(
             }
             SimpleButton(name = "Create") {
                 viewModel.createHousehold()
+            }
+            if(viewModel.household.collectAsState().value != null)
+            {
+                QrCodeView(
+                    data = viewModel.household.collectAsState().value!!.uuid,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .fillMaxWidth()
+                )
             }
         }
     }
--- app/src/main/java/at/ac/fhcampuswien/budget_fox/view_models/HouseholdCreateViewModel.kt
+++ app/src/main/java/at/ac/fhcampuswien/budget_fox/view_models/HouseholdCreateViewModel.kt
@@ -4,14 +4,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import at.ac.fhcampuswien.budget_fox.data.UserRepository
 import at.ac.fhcampuswien.budget_fox.models.Household
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class HouseholdCreateViewModel : ViewModel() {
 
     private val userRepository = UserRepository()
 
     private var _householdName = mutableStateOf(value = "").value
+
+    private var _household = MutableStateFlow<Household?>(mutableStateOf(value = null).value)
     val householdName: String
         get() = _householdName
+
+    val household: StateFlow<Household?>
+        get() = _household.asStateFlow()
 
     fun setHouseholdName(householdName: String) {
         _householdName = householdName
@@ -19,6 +27,7 @@ class HouseholdCreateViewModel : ViewModel() {
 
     fun createHousehold() {
         val household = Household(name = _householdName)
+        _household.value = household
 
         userRepository.insertHousehold(household = household)
     }
