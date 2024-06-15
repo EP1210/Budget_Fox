f8e631daa928095899be04abee4ee01e5b78bae0
EP1210 <erik.pfandler@stud.fh-campuswien.ac.at>
2024-06-15 20:15:40 +0200

    Added colour as parameter to SimpleEventIcon widget

--- app/src/main/java/at/ac/fhcampuswien/budget_fox/widgets/CategoryItem.kt
+++ app/src/main/java/at/ac/fhcampuswien/budget_fox/widgets/CategoryItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
@@ -44,7 +45,8 @@ fun CategoryItem(
                     edit()
                 }
                 SimpleEventIcon(
-                    icon = Icons.Default.Delete
+                    icon = Icons.Default.Delete,
+                    colour = Color.Red
                 ) {
                     delete()
                 }
--- app/src/main/java/at/ac/fhcampuswien/budget_fox/widgets/SimpleEventIcon.kt
+++ app/src/main/java/at/ac/fhcampuswien/budget_fox/widgets/SimpleEventIcon.kt
@@ -4,12 +4,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
 fun SimpleEventIcon(
     icon: ImageVector,
     modifier: Modifier = Modifier,
+    colour: Color = Color.Unspecified,
     event: () -> Unit
 ) {
     IconButton(
@@ -18,7 +20,8 @@ fun SimpleEventIcon(
     ) {
         Icon(
             imageVector = icon,
-            contentDescription = "Localized description"
+            contentDescription = "Localized description",
+            tint = colour
         )
     }
 }
