75fc127456c125ce20fe06818433d5d5b7a322fd
michael-mezgo <58122000+michael-mezgo@users.noreply.github.com>
2024-07-06 23:32:39 +0200

    Saving goals can be edited #18

--- app/src/main/java/at/ac/fhcampuswien/budget_fox/screens/SavingGoalOverviewScreen.kt
+++ app/src/main/java/at/ac/fhcampuswien/budget_fox/screens/SavingGoalOverviewScreen.kt
@@ -1,6 +1,7 @@
 package at.ac.fhcampuswien.budget_fox.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,6 +14,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +28,9 @@ import at.ac.fhcampuswien.budget_fox.navigation.Screen
 import at.ac.fhcampuswien.budget_fox.view_models.SavingGoalOverviewViewModel
 import at.ac.fhcampuswien.budget_fox.view_models.ViewModelFactory
 import at.ac.fhcampuswien.budget_fox.widgets.SavingGoalListItem
+import at.ac.fhcampuswien.budget_fox.widgets.SimpleButton
+import at.ac.fhcampuswien.budget_fox.widgets.SimpleField
+import at.ac.fhcampuswien.budget_fox.widgets.SimpleNumberField
 import at.ac.fhcampuswien.budget_fox.widgets.SimpleTopAppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -65,9 +70,18 @@ fun SavingGoalOverviewScreen(
                 .fillMaxSize()
         ) {
             items(items = savingGoalItems.value) { item ->
-                SavingGoalListItem(savingGoal = item) {
-                    navController.navigate(Screen.SavingGoalTransactionList.setArguments(userId = userId, savingGoalId = item.uuid))
-                }
+                SavingGoalListItem(savingGoal = item, onClick = {
+                    navController.navigate(
+                        Screen.SavingGoalTransactionList.setArguments(
+                            userId = userId,
+                            savingGoalId = item.uuid
+                        )
+                    )
+                },
+                    onEdit = { goalId ->
+                        viewModel.loadSelectedGoal(goalId = goalId)
+                        viewModel.setBottomSheetVisible(visible = true)
+                    })
             }
         }
         Box(
@@ -86,6 +100,50 @@ fun SavingGoalOverviewScreen(
                     .align(alignment = Alignment.BottomEnd)
             ) {
                 Icon(Icons.Filled.Add, "Add goal")
+            }
+        }
+        if (viewModel.bottomSheetVisible.value) {
+            ModalBottomSheet(onDismissRequest = {
+                viewModel.setBottomSheetVisible(visible = false)
+                viewModel.selectedGoal.value = null
+                viewModel.setGoalAmount(0.0)
+                viewModel.setGoalName("")
+            }) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Text(
+                        text = "Edit saving goal",
+                        modifier = Modifier
+                            .padding(bottom = 10.dp)
+                    )
+                    SimpleField(
+                        title = "Name",
+                        defaultValue = viewModel.selectedGoal.value?.name ?: ""
+                    ) { name ->
+                        viewModel.setGoalName(name)
+                    }
+                    SimpleNumberField(
+                        title = "Amount",
+                        textDefaultValue = viewModel.selectedGoal.value?.amount.toString()
+                    ) { amount ->
+                        if(amount.isNotEmpty()) {
+                            viewModel.setGoalAmount(amount.toDouble())
+                        }
+                    }
+                    SimpleButton(
+                        name = "Edit goal"
+                    ) {
+                        viewModel.saveEditedGoal(userId = userId, onSuccess = {
+                            viewModel.setBottomSheetVisible(visible = false)
+                            viewModel.selectedGoal.value = null
+                            viewModel.setGoalAmount(0.0)
+                            viewModel.setGoalName("")
+                        })
+                    }
+                }
             }
         }
     }
--- app/src/main/java/at/ac/fhcampuswien/budget_fox/widgets/SavingGoalListItem.kt
+++ app/src/main/java/at/ac/fhcampuswien/budget_fox/widgets/SavingGoalListItem.kt
@@ -2,7 +2,7 @@ package at.ac.fhcampuswien.budget_fox.widgets
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,7 +29,8 @@ import at.ac.fhcampuswien.budget_fox.models.Transaction
 @Composable
 fun SavingGoalListItem(
     savingGoal: SavingGoal,
-    onClick: () -> Unit = {}
+    onClick: () -> Unit = {},
+    onEdit: (String) -> Unit = {}
 ) {
     val currentProgress by remember { mutableFloatStateOf((savingGoal.getProgress() / savingGoal.amount).toFloat()) }
     Column(
@@ -38,8 +40,16 @@ fun SavingGoalListItem(
             .border(0.dp, Color.Transparent)
             .clip(RoundedCornerShape(6.dp))
             .background(MaterialTheme.colorScheme.primary)
-            .clickable {
-                onClick()
+
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onLongPress = {
+                        onEdit(savingGoal.uuid)
+                    },
+                    onTap = {
+                        onClick()
+                    }
+                )
             }
             .padding(16.dp),
     ) {
